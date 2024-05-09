46e5faa14a221e3e47a1ca19125f9462f08f1548
gabriel rodrigues <gabriel.rtlima@upe.br>
2024-05-09 20:29:03 -0300

    feat: setup header

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "gabrielrtl",
+  url: "https://gabrielrtlima.dev",
+  description: "My personal blog about web development and programming.",
+  links: {
+    github: "https://github.com/gabrielrtlima",
+    twitter: "https://twitter.com/bielrt1",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;

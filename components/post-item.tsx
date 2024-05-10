4b7189adbafd196504af10356f87614b1a614758
gabriel rodrigues <gabriel.rtlima@upe.br>
2024-05-10 11:42:41 -0300

    style: remove description

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -14,15 +14,15 @@ export function PostItem({ slug, title, description, date }: PostItemProps) {
   return (
     <article className="flex flex-col gap-2 border-border border-b py-3">
       <div>
-        <h2 className="text-2xl font-bold">
+        <h2 className="text-2xl font-normal">
           <Link href={slug}>{title.toLowerCase()}</Link>
         </h2>
       </div>
-      <div className="max-w-none text-muted-foreground">{description}</div>
+      {/* <div className="max-w-none text-muted-foreground">{description}</div> */}
       <div className="flex justify-between items-center">
         <dl>
           <dt className="sr-only">Published On</dt>
-          <dd className="text-sm sm:text-base font-medium flex items-center gap-1">
+          <dd className="text-sm sm:text-base font-normal flex items-center gap-1">
             <Calendar className="w-4 h-4" />
             <time dateTime={date}>{formatDate(date)}</time>
           </dd>

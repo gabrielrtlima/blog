4866eab6a9de526f3e5cf1ad549958d9008253e3
gabriel rodrigues <gabriel.rtlima@upe.br>
2024-05-10 07:09:48 -0300

    style: change home title

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,31 @@ import { posts } from "@/.velite";
 import { PostItem } from "@/components/post-item";
 import { siteConfig } from "@/config/site";
 import { sortPosts } from "@/lib/utils";
+import { useState } from "react";
 import Typewriter from "typewriter-effect";
 
 export default function Home() {
+  const [isCompleted, setIsCompleted] = useState(false);
   const latestPosts = sortPosts(posts).slice(0, 3);
   return (
     <>
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
         <div className="container flex flex-col gap-4 text-center">
           <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-black text-balance">
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter
-                  .typeString("<strong>leirbag</strong>")
-                  .pauseFor(300)
-                  .deleteAll()
-                  .typeString("<strong>gabriel</strong>")
-                  .start();
-              }}
-            />
+            {!isCompleted ? (
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString("<strong>leirbag</strong>")
+                    .pauseFor(300)
+                    .deleteAll()
+                    .typeString("<strong>gabriel</strong>")
+                    .start()
+                    .callFunction(() => setIsCompleted(true));
+                }}
+              />
+            ) : null}
+            {isCompleted && "gabriel"}
           </h1>
           <p className="max-w-[42rem] mt-2 mx-auto text-muted-foreground sm:text-xl text-balance">
             {siteConfig.description}

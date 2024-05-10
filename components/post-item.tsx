b5562b97153b2896cc0f3d171ae6378605ba05ee
gabriel rodrigues <gabriel.rtlima@upe.br>
2024-05-10 06:22:47 -0300

    chore: erase posts

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -15,7 +15,7 @@ export function PostItem({ slug, title, description, date }: PostItemProps) {
     <article className="flex flex-col gap-2 border-border border-b py-3">
       <div>
         <h2 className="text-2xl font-bold">
-          <Link href={slug}>{title}</Link>
+          <Link href={slug}>{title.toLowerCase()}</Link>
         </h2>
       </div>
       <div className="max-w-none text-muted-foreground">{description}</div>
